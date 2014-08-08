80541e1ad2e8eecc09920cd777764d2bbb5ebb62
markbencze <markbencze@gmail.com>
2014-08-07 22:48:11 -0400

    android_device_samsung_hlte

---
+++ android_device_samsung_hlte/jedi.mk
@@ -0,0 +1,10 @@
+$(call inherit-product, device/samsung/hlte/full_hlte.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
+
+# Inherit some common JEDI stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+PRODUCT_DEVICE := hlte
+PRODUCT_NAME := JEDI_hlte
